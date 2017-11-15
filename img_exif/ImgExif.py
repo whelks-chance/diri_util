769fe05ffb1ef42549cd61c86465a513cfa77e63
Ian Harvey <iharvey@asb360.com>
2017-11-15 17:54:14 +0000

    parsing lat lngs

--- img_exif/ImgExif.py
+++ img_exif/ImgExif.py
@@ -5,6 +5,40 @@ import dateutil.parser
 
 class ImgExif:
 
+    def exif_latlng_to_wgs84(self, lat, lng, lat_dir, lng_dir):
+        lat_arr = str(lat).replace('[', '').replace(']', '').split(',')
+        lat_arr_final = []
+        for lat_part in lat_arr:
+            if '/' in lat_part:
+                fraction_parts = lat_part.split('/')
+                lat_part = str(
+                    round(
+                        float(fraction_parts[0]) / float(fraction_parts[1])
+                    )
+                )
+            lat_arr_final.append(lat_part.strip())
+        dd = float(lat_arr_final[0]) + float(lat_arr_final[1]) / 60 + float(lat_arr_final[2]) / (60 * 60);
+
+        print('lat', dd, 'is S == *{}*'.format(lat_dir), lat_dir == "S")
+        if str(lat_dir) == 'S':
+            dd *= -1
+
+        lng_arr = str(lng).replace('[', '').replace(']', '').split(',')
+        lng_arr_final = []
+        for lng_part in lng_arr:
+            if '/' in lng_part:
+                fraction_parts = lng_part.split('/')
+                lng_part = str(
+                    round(
+                        float(fraction_parts[0]) / float(fraction_parts[1])
+                    )
+                )
+            lng_arr_final.append(lng_part.strip())
+        d_lng = float(lng_arr_final[0]) + float(lng_arr_final[1]) / 60 + float(lng_arr_final[2]) / (60 * 60);
+        if str(lng_dir) == 'W':
+            d_lng *= -1
+        return dd, d_lng
+
     def exif_date_to_iso8601(self, gps_date, gps_timestamp):
 
         gps_timestamp = str(gps_timestamp).replace('[', '').replace(']', '')
@@ -72,6 +106,15 @@ class ImgExif:
                     print('\n', 'datetime.datetime object:', time_obj)
                     print(time_str)
 
+                if 'GPS GPSLongitude' in tags.keys():
+                    lat, lng = self.exif_latlng_to_wgs84(
+                        tags['GPS GPSLatitude'],
+                        tags['GPS GPSLongitude'],
+                        tags['GPS GPSLatitudeRef'],
+                        tags['GPS GPSLongitudeRef'],
+                    )
+                    print(lat, lng)
+
                 print(len(gps_keys))
                 print('x'*20, '\n\n')
 
@@ -79,3 +122,11 @@ if __name__ == '__main__':
     img_location = "C:\\Users\\Ian Harvey\\OneDrive - Cardiff University\\Email attachments"
     ie = ImgExif()
     ie.read_exif(img_location)
+
+    # ie.exif_latlng_to_wgs84(
+    #     "[6, 11, 3929/100]",
+    #     "[106, 49, 2351/100]",
+    #     "S",
+    #     "E"
+    # )
+
